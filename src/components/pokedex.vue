9f2689bad12ecdd0a88ebd2aadcca4bdcec39b61
Alfred Morgan <Amorgan>
2020-03-12 20:43:26 -0400

    added Pokemon name, Pokedex ID, Need to figure out better design layout

--- src/components/pokedex.vue
+++ src/components/pokedex.vue
@@ -1,7 +1,11 @@
 <template>
   <v-container class="text-center justify-center">
-    <v-card max-width="400" v-if="pokemon != null">
+    <v-card min-width="200" max-width="600" v-if="pokemon != null">
       <v-btn v-if="pokemon_id != 1" color="primary" @click="pokemon_id = pokemon_id - 1; get_pokemon()">-</v-btn>
+      <v-list-item-content>
+              <v-list-item-title class="title">{{pokemon.name}}</v-list-item-title>
+              <v-list-item-subtitle>Pokedex ID: {{pokemon.id}}</v-list-item-subtitle>
+      </v-list-item-content>
       <v-avatar size="133">
         <img v-bind:src="pokemon.sprites.front_default">
       </v-avatar>
@@ -20,15 +24,21 @@ const PokemonRepository = RepositoryFactory.get('pokemon')
     data: () => ({
       pokemon_id: 1,
       pokemon: null,
+      species: null,
     }),
     methods:{
       async get_pokemon(){
         const { data } = await PokemonRepository.get_pokemon(this.pokemon_id)
         this.pokemon = data;
+      },
+      async get_species(){
+        const { data } = await PokemonRepository.get_species(this.pokemon_id)
+        this.species = data;
       }
     },
     mounted(){
       this.get_pokemon();
+      this.get_species();
     }
   }
 </script>
--- src/repositorys/pokemonRepository.js
+++ src/repositorys/pokemonRepository.js
@@ -4,5 +4,8 @@ const resource = "/pokemon";
 export default {
     get_pokemon(id){
         return Repository.get(`${resource}/${id}`);
+    },
+    get_species(id){
+        return Repository.get(`${resource}-species/${id}`);
     }
 }
