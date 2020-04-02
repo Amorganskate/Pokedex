0bc0766ebb3fb79f15ebb03dff14ccfbe8f616d2
Alfred Morgan <Amorgan>
2020-04-02 11:53:05 -0400

    netlify blocks if request isnt https

--- src/repositorys/Repository.js
+++ src/repositorys/Repository.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseDomain = "http://pokeapi.co";
+const baseDomain = "https://pokeapi.co";
 const baseURL = `${baseDomain}/api/v2/`
 
 export default axios.create({
